5581bfaea86325fddab0e3e1b7b4ba39200c41b8
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-19 21:39:31 +0800

    Time: 69 ms (43.32%), Space: 44.6 MB (75.30%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -2,7 +2,7 @@ type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
     let result = init
-    for (let num of nums) {
+    for (const num of nums) {
         result = fn(result, num)
     }
     return result
