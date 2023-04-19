9f94f7722503949327db00ccbafca97465dcabcc
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-19 21:28:15 +0800

    Time: 54 ms (89.70%), Space: 42.3 MB (91.08%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.ts
@@ -0,0 +1,14 @@
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    return this.at(-1) !== undefined ? this.at(-1) : -1
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
