faf70ed99178ce97f14a1bbef91b85c536870037
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-19 21:32:20 +0800

    Time: 51 ms (96.99%), Space: 44.5 MB (19.92%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
@@ -0,0 +1,7 @@
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) result.push(arr[i])
+    }
+    return result
+};
