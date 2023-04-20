c0b270c71428e3fc2f678288ce0ee9f752a4c6d9
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-20 21:06:19 +0800

    Time: 58 ms (86.84%), Space: 15.1 MB (56.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, n in enumerate(nums):
+            find = target - n
+            if find in map:
+                return [map[find],i]
+            map[n] = i
