93ed08172fdeef4b8b9ceff08ffc0fcfbc315757
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-27 23:36:20 +0800

    Time: 173 ms (5.11%), Space: 16.4 MB (37.74%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        ans = 0
+        for i in range(1,n+1,1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                print(i)
+                ans += i
+        return ans
