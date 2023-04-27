8da787816419fe714bd73645dfd9f91d72fd18bf
Vi <86089777+vivo1310@users.noreply.github.com>
2023-04-27 23:36:49 +0800

    Time: 99 ms (8.67%), Space: 16.3 MB (37.74%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -3,6 +3,5 @@ class Solution:
         ans = 0
         for i in range(3,n+1,1):
             if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
-                print(i)
                 ans += i
         return ans
