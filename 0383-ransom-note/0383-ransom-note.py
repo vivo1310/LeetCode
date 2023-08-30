ec877af668359ca04525146932ed1c8aa91a7dc4
Vi <86089777+vivo1310@users.noreply.github.com>
2023-08-30 19:01:52 +0800

    Time: 78 ms (33.05%), Space: 16.5 MB (91.39%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        m = {} # magazine letter occurence
+        for c in magazine:
+            if c not in m:
+                m[c] = 1
+            else:
+                m[c] += 1
+        n = {} # ransomNote letter occurence
+        for c in ransomNote:
+            if c not in n:
+                n[c] = 1
+            else:
+                n[c] += 1
+#         loop thru m, check occ of each letter is the same in ransomNote occ
+        for l in n.keys():
+            if m.get(l) is None or m[l] < n[l]:
+                return False
+        return True
+    
