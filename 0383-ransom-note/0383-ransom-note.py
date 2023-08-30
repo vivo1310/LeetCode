8ccee69d4fc69cf3128d61df81cc06849b6d9735
Vi <86089777+vivo1310@users.noreply.github.com>
2023-08-30 19:04:25 +0800

    Time: 62 ms (69.72%), Space: 16.5 MB (91.39%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,27 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        m = {} # magazine letter occurence
-        for c in magazine:
-            if c not in m:
-                m[c] = 1
-            else:
-                m[c] += 1
-        n = {} # ransomNote letter occurence
-        for c in ransomNote:
-            if c not in n:
-                n[c] = 1
-            else:
-                n[c] += 1
-#         loop thru m, check occ of each letter is the same in ransomNote occ
-        for l in n.keys():
-            if m.get(l) is None or m[l] < n[l]:
+#         m = {} # magazine letter occurence
+#         for c in magazine:
+#             if c not in m:
+#                 m[c] = 1
+#             else:
+#                 m[c] += 1
+#         n = {} # ransomNote letter occurence
+#         for c in ransomNote:
+#             if c not in n:
+#                 n[c] = 1
+#             else:
+#                 n[c] += 1
+# #         loop thru m, check occ of each letter is the same in ransomNote occ
+#         for l in n.keys():
+#             if m.get(l) is None or m[l] < n[l]:
+#                 return False
+#         return True
+        m = Counter(magazine)
+        rn = Counter(ransomNote)
+        for i in rn:
+            if i not in m or m[i] < rn[i]:
                 return False
+            
         return True
-    
