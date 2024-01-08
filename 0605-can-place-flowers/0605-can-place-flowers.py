7355759659c27655b7f58dd67ae8013e0b76e49f
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-08 22:38:46 +0700

    Time: 143 ms (72.50%), Space: 17.6 MB (29.84%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,7 +1,6 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # 1 0 1 0 0 0 0 0 0 n = 3
-        count = 0 # number of flowers that could be planted
+        # count = 0 # number of flowers that could be planted
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0: # check if this is empty slot
                 leftEmpty = (i == 0) or flowerbed[i-1] == 0
@@ -9,6 +8,20 @@ class Solution:
                 if leftEmpty and rightEmpty:
 #                     plant the flower in the empty slot
                     flowerbed[i] = 1
-                    count += 1
+                    n -= 1
+                    # count += 1
         # if n < number of flowers that could be planted, return True
-        return count >= n
+        # return count >= n
+        return n <= 0
+    
+    
+    
+#     Instead of using that super long 'if condition'. Its better to add 0 at the start and end of the list. Makes it cleaner and easier to understand!
+#         bed = [0] + flowerbed + [0]
+#         for i in range(1, len(flowerbed) + 1):
+#             if bed[i] == bed[i-1] == bed[i+1] == 0:
+# #                 plant it
+#                 bed[i] = 1
+#                 n -= 1
+#             if n <= 0: return True
+#         return False
