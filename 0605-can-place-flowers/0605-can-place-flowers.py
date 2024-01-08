c7ab4dac62344ef8e91d11e01f79df759106f9e2
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-08 22:39:03 +0700

    Time: 138 ms (82.48%), Space: 17.8 MB (11.12%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,27 +1,27 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         # count = 0 # number of flowers that could be planted
-        for i in range(len(flowerbed)):
-            if flowerbed[i] == 0: # check if this is empty slot
-                leftEmpty = (i == 0) or flowerbed[i-1] == 0
-                rightEmpty = (i == len(flowerbed) - 1) or flowerbed[i+1] == 0
-                if leftEmpty and rightEmpty:
-#                     plant the flower in the empty slot
-                    flowerbed[i] = 1
-                    n -= 1
-                    # count += 1
-        # if n < number of flowers that could be planted, return True
-        # return count >= n
-        return n <= 0
+#         for i in range(len(flowerbed)):
+#             if flowerbed[i] == 0: # check if this is empty slot
+#                 leftEmpty = (i == 0) or flowerbed[i-1] == 0
+#                 rightEmpty = (i == len(flowerbed) - 1) or flowerbed[i+1] == 0
+#                 if leftEmpty and rightEmpty:
+# #                     plant the flower in the empty slot
+#                     flowerbed[i] = 1
+#                     n -= 1
+#                     # count += 1
+#         # if n < number of flowers that could be planted, return True
+#         # return count >= n
+#         return n <= 0
     
     
     
 #     Instead of using that super long 'if condition'. Its better to add 0 at the start and end of the list. Makes it cleaner and easier to understand!
-#         bed = [0] + flowerbed + [0]
-#         for i in range(1, len(flowerbed) + 1):
-#             if bed[i] == bed[i-1] == bed[i+1] == 0:
-# #                 plant it
-#                 bed[i] = 1
-#                 n -= 1
-#             if n <= 0: return True
+        bed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed) + 1):
+            if bed[i] == bed[i-1] == bed[i+1] == 0:
+#                 plant it
+                bed[i] = 1
+                n -= 1
+            if n <= 0: return True
 #         return False
