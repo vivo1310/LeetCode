b86d35517f8b871c2df1420baac13b92145a8b39
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-09 15:25:40 +0700

    Time: 47 ms (99.43%), Space: 18.6 MB (15.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
+        # time: O(n), space: O(n)
+        # map = {}
+        # for i, n in enumerate(nums):
+        #     find = target - n
+        #     if find in map:
+        #         return [map[find],i]
+        #     map[n] = i
+        
+        # use enumerate to get the index and the value of the array's item
+        hashMap = dict() # val:index
         for i, n in enumerate(nums):
-            find = target - n
-            if find in map:
-                return [map[find],i]
-            map[n] = i
+            m = target - n
+            if m in hashMap:
+                return [hashMap[m], i] # i is current index, hashMap[m] is index of m in nums array
+            hashMap[n] = i
