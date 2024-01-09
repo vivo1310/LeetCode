2de1a7d515c6db4ecd66a2a650a720982f8e2143
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-09 15:16:21 +0700

    Time: 430 ms (87.46%), Space: 32.9 MB (28.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hmap = {}
+        # hmap = {}
+        # for n in nums:
+        #     if n not in hmap:
+        #         hmap[n] = 1
+        #     else:
+        #         return True
+        # return False
+#       another way is to use hashset
+        hashset = set()
         for n in nums:
-            if n not in hmap:
-                hmap[n] = 1
-            else:
+            if n in hashset: # number is already existed in the hashset
                 return True
+            hashset.add(n)
         return False
-        
