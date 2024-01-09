668c9dd49da1df136d17b25cdcdcc463996d49c6
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-09 16:07:24 +0700

    Time: 337 ms (97.40%), Space: 32.6 MB (28.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,7 +3,6 @@ class Solution:
         # time: O(n), space: O(n) for the set
         longestLength = 0
         if not nums: return longestLength
-
         nSet = set(nums)
 #       loop thru nums, check if n is the start of the sequence
 #       if yes, check for next consecutive, until no more
@@ -12,27 +11,12 @@ class Solution:
             length = 0
             if n-1 not in nSet: # it is the start of the sequence
                 length += 1
-                i = 1
-                # print(n, length)
-                while n+i in nSet:
+                # i = 1
+                while n+length in nSet:
                     length += 1
-                    i += 1
-                    # print(n, length)
-
-                if length >= longestLength: longestLength = length
-        # print(longestLength)
+                    # i += 1
+                # if length >= longestLength: longestLength = length
+                longestLength = max(length, longestLength) #shorter than the aboe line
             
         return longestLength
-        # if not nums: return 0
-        # # create the map
-        # hashMap = {} # n : n+1
-        # for n in nums:
-        #     hashMap[n] = n+1
-        # # check for consecutive element
-        # length = 1 # there's always a min length of 1 if there's a number in nums array
-        # print(hashMap)
-        # for k in hashMap:
-        #     if hashMap[k] in hashMap.keys():
-        #         print(k, hashMap[k])
-        #         length += 1
-        # return length
+    
