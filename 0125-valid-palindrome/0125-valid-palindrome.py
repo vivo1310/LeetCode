82e0a26aaa3f7aa511d62b89b90498a7e704f96e
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-10 11:04:48 +0700

    Time: 63 ms (21.07%), Space: 17.7 MB (51.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # .isalnum() check if a character is alphanumeric
+        # If we don't wanna use built-in func .isalnum, we can use ascii order to check
+        def isAlphaNum(c):
+            return ((ord('A') <= ord(c) <= ord('Z')) or 
+                    (ord('a') <= ord(c) <= ord('z')) or 
+                    (ord('0') <= ord(c) <= ord('9')))
         # set 2 pointers, 1 at the start, 1 at the end of the string
         # if the char at the pointer index is a alphanumeric char 
         # if 2 char is the same, move to the next index, repeat checking til 2 pointers meet
+        # 1st Sol: remove all non-alphanum char from - memory O(n) n is length of str
         # string = ''
-        # # remove all non-alphanum char from - memory O(n) n is length of str
+        # 
         # for c in s.lower():
         #     if c.isalnum():
         #         string += c
@@ -18,12 +24,14 @@ class Solution:
         #     j -= 1
         # return True
     
-    
+#     Sol 2: no additional memory
         i = 0
         j = len(s) - 1
         while i < j:
-            firstAlnumCheck = s[i].isalnum()
-            secondAlnumCheck = s[j].isalnum()
+            # firstAlnumCheck = s[i].isalnum()
+            # secondAlnumCheck = s[j].isalnum()
+            firstAlnumCheck = isAlphaNum(s[i])
+            secondAlnumCheck =  isAlphaNum(s[j])
             if firstAlnumCheck and secondAlnumCheck:
                 if s[i].lower() != s[j].lower():
                     return False
@@ -35,4 +43,5 @@ class Solution:
             elif not secondAlnumCheck:
                 j -= 1
         return True
+   
         
