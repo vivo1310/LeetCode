dcd1e1fc1ff7b4c9f4bb6cf53dc24de739519653
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-10 10:37:44 +0700

    Time: 78 ms (11.22%), Space: 18 MB (30.23%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # .isalnum() check if a character is alphanumeric
+        # set 2 pointers, 1 at the start, 1 at the end of the string
+        # if the char at the pointer index is a alphanumeric char 
+        # if 2 char is the same, move to the next index, repeat checking til 2 pointers meet
+        string = ''
+        # remove all non-alphanum char from
+        for c in s.lower():
+            if c.isalnum():
+                string += c
+        print(string)
+        i = 0
+        j = len(string) - 1
+        while i < j:
+            print(i,j,string[i], string[j])
+            if string[i] != string[j]: 
+                return False
+            i += 1
+            j -= 1
+        return True
+        
