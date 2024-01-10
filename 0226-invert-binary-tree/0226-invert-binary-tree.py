8c58f1a0bdd19d89a8d48e7428e1cbdfb579e44b
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-10 16:25:41 +0700

    Time: 26 ms (98.74%), Space: 17.2 MB (32.05%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -9,21 +9,28 @@ class Solution:
         # take a parent node, look at the children, swap them
         # now a child node become parent node, repeat that until no more child
         # q is the queue of parent nodes
-        # print(root)
-        q = [root]
-        while q:
-            parent = q.pop()
-            # print('parent', parent)
-            # add the children to the q waiting to be traversed
-            if parent != None:
-                if parent.left != None:
-                    q.append(parent.left)
-                if parent.right != None:
-                    q.append(parent.right)
-
-                # print(q)
-                # swap the children
-                parent.left, parent.right = parent.right, parent.left
+#         if not root:
+#             return None
+#         q = [root]
+#         while q:
+#             parent = q.pop()
+#             # add the children to the q waiting to be traversed
+#             if parent != None:
+#                 if parent.left != None:
+#                     q.append(parent.left)
+#                 if parent.right != None:
+#                     q.append(parent.right)
+#                 # swap the children
+#                 parent.left, parent.right = parent.right, parent.left
             
-        return root
+#         return root
         
+        # Sol2: recursion
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left # swap
+        
+        # repeat for left and right sub-tree
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
