2b6c08bfb29f85819720aad6e79f2e1bcc3e7118
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-10 10:57:07 +0700

    Time: 44 ms (82.25%), Space: 17.8 MB (51.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,17 +4,48 @@ class Solution:
         # set 2 pointers, 1 at the start, 1 at the end of the string
         # if the char at the pointer index is a alphanumeric char 
         # if 2 char is the same, move to the next index, repeat checking til 2 pointers meet
-        string = ''
-        # remove all non-alphanum char from
-        for c in s.lower():
-            if c.isalnum():
-                string += c
+        # string = ''
+        # # remove all non-alphanum char from - memory O(n) n is length of str
+        # for c in s.lower():
+        #     if c.isalnum():
+        #         string += c
+        # i = 0
+        # j = len(string) - 1
+        # while i < j:
+        #     if string[i] != string[j]: 
+        #         return False
+        #     i += 1
+        #     j -= 1
+        # return True
+    
+    
         i = 0
-        j = len(string) - 1
+        j = len(s) - 1
         while i < j:
-            if string[i] != string[j]: 
-                return False
-            i += 1
-            j -= 1
+            # firstAlnum = s[i].isalnum()
+            # secondAlnum = s[j].isalnum()
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            # check for alnum
+#             if not s[i].isalnum(): 
+#                 i += 1
+#             if not s[j].isalnum():
+#                 j -= 1
+                
+#             print(i,j,s[i],s[j])
+#             # check for palindrom
+#             if s[i].lower() != s[j].lower():
+#                 return False
+#             else:
+#                 i += 1
+#                 j -= 1
         return True
         
