004e3563116dcbb7231c792ef53d9cb50f419f9c
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-11 11:12:16 +0700

    Time: 34 ms (83.25%), Space: 17.4 MB (19.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+ 
+        hashMap = {
+            "(":")",
+            "[":"]",
+            "{":"}"
+        }
+        stack = []
+        
+        for c in s:
+            if c in hashMap: # check if char is an open bracket
+                stack.append(c)
+            else:
+                if stack == []:
+                    return False
+                last = stack.pop()
+                if c != hashMap[last]:
+                    return False
+        if stack == []: return True
+        return False
+        # return True
