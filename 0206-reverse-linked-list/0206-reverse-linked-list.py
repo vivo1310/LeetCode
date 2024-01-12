3f711570f7fb71f19b4562178edb2986f0500f17
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-12 23:03:54 +0700

    Time: 37 ms (81.62%), Space: 18.6 MB (18.50%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,13 +6,12 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Recursive O(n) memory O(n) cuz creating newHead
-        if not head:
+
+        if not head or not head.next:
             return head
-        newHead = head
-        if head.next: # if we still have sub-problem
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-            head.next = None
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
         return newHead
     
     
