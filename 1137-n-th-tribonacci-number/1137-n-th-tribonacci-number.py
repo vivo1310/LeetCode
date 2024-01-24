6eed9a0104d00bdec65a06d4edc2dc78d515f007
Vi <86089777+vivo1310@users.noreply.github.com>
2024-01-25 01:08:49 +0800

    Time: 33 ms (80.24%), Space: 16.5 MB (60.61%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,14 +1,34 @@
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+# #         DP - Tabulation, iteratively calc bottom up
+#         t = {}
+#         t[0] = 0
+#         t[1] = 1
+#         t[2] = 1
+#         if n in t:
+#             return t[n]
+        
+#         for i in range(3, n + 1):
+#             t[i] = t[i-1] + t[i-2] + t[i-3]
+            
+#         return t[n]
+
+# DP - memoization
 class Solution:
     def tribonacci(self, n: int) -> int:
-#         DP - Tabulation
-        t = {}
-        t[0] = 0
-        t[1] = 1
-        t[2] = 1
-        if n in t:
-            return t[n]
+        t = [-1 for i in range(n + 1)]
         
-        for i in range(3, n + 1):
-            t[i] = t[i-1] + t[i-2] + t[i-3]
-            
-        return t[n]
+        def cache(x, t):
+            if x == 0:
+                return 0
+            if x == 1:
+                return 1
+            if x == 2:
+                return 1
+            if t[x] != -1: # solution found
+                return t[x]
+            # otherwise, calculate t[x] = t[x-1] + t[x-2] + t[x-3] recursively
+            t[x] = cache(x-1, t) + cache(x-2, t) +  cache(x-3, t)
+            return t[x]
+        
+        return cache(n, t)
