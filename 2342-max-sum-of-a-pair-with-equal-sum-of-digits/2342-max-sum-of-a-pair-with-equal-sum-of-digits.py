40ee0094007ec73f383f49ff9fa892810e770d27
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-07 16:23:48 +0700

    Time: 693 ms (90.62%), Space: 31.2 MB (88.11%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -14,51 +14,11 @@ class Solution:
             if digitSum not in hashMap:
                 hashMap[digitSum] = n
             else:
+                # set res only when we have a equal digit sum pair
                 res = max(hashMap[digitSum] + n , res)
                 hashMap[digitSum] = max(n, hashMap[digitSum])
         return res
             
         
         
-        # Initialize a dictionary to store the maximum number for each digit sum
-
-        digit_sum_max_num = defaultdict(int)
-
-
-        # Iterate through each number in the given list
-
-        for num in nums:
-
-            # Calculate the sum of digits for the current number
-
-            digit_sum = 0
-
-            temp_num = num
-
-            while temp_num:
-
-                digit_sum += temp_num % 10
-
-                temp_num //= 10
-
-
-            # If the sum of digits has been seen before,
-
-            # check if the current number contributes to a larger max sum
-
-            if digit_sum in digit_sum_max_num:
-
-                max_sum = max(max_sum, digit_sum_max_num[digit_sum] + num)
-
-          
-
-            # Update the maximum number for the current digit sum
-
-            digit_sum_max_num[digit_sum] = max(digit_sum_max_num[digit_sum], num)
-
-      
-
-        # Return the maximum sum of two numbers having the same sum of digits
-
-        return max_sum
                 
