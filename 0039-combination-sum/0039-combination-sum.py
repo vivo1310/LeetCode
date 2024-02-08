07e2ad966b70d8bb5906a67b9d6f94f6e1d3eafc
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-08 16:13:02 +0700

    Time: 50 ms (77.55%), Space: 16.5 MB (83.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,7 +4,7 @@ class Solution:
         def backtrack(start = 0, total = 0, comb = []):
             if target == total:
                 res.append(comb.copy())
-                
+                return
             else:
                 for i in range(start, len(candidates)):
                     if total + candidates[i] <= target:
