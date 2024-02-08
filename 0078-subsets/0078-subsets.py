b002548e33e8df4ee5e8a41ac740362ffd9dc36c
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-08 15:39:44 +0700

    Time: 43 ms (31.79%), Space: 16.9 MB (54.92%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        subsets = [[], [nums[0]]]
-        i = 1
-        while i < len(nums):
-            temp = []
-            for element in subsets:
-                temp.append(element) # dont include
-                temp.append(element + [nums[i]]) # include
-            subsets = temp
-            i += 1
-        return subsets
+        # Method 1
+        # subsets = [[], [nums[0]]]
+        # i = 1
+        # while i < len(nums):
+        #     temp = []
+        #     for element in subsets:
+        #         temp.append(element) # dont include
+        #         temp.append(element + [nums[i]]) # include
+        #     subsets = temp
+        #     i += 1
+        # return subsets
     
     #     include 1?
     #         yes.[1]                                   no.[]
@@ -20,3 +21,23 @@ class Solution:
     #     include 3?          |     \             |     \           |    \
     #         |    \          |      \            |      \          |     \
     # yes.[1,2,3]  no.[1]   yes.[1,3] no.[1]   yes.[2,3] no.[2]    yes.[3] no.[]
+    
+        # Method 2: backtracking
+        res = []
+        subset = []
+
+        def backtrack(i = 0):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            # decision to include
+            subset.append(nums[i])
+            backtrack(i + 1)
+
+            # decision not to include
+            subset.pop()
+            backtrack(i+1)
+
+        backtrack()
+        return res
