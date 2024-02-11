63f5a1b5cd1c965064bbf0c4862094ed26062b42
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-11 14:54:10 +0700

    Time: 33 ms (87.03%), Space: 16.8 MB (68.28%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # open, close = 0, 0
+        res = []
+        
+        def backtrack(comb = "", open = 0, close = 0):
+            if open == n and close == n:
+                res.append(comb)
+                return
+            
+            if open < n:
+                comb += "("
+                # open += 1
+                backtrack(comb, open + 1, close)
+                comb = comb[:-1]
+
+            if open > close:
+                comb += ")"
+                # close += 1
+                backtrack(comb, open, close + 1)
+                comb = comb[:-1]
+
+        backtrack()
+        return res
