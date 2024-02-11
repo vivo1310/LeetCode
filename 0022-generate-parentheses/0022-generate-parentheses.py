e51b9f0ff82dd974e4b1519a97c946ad452b1840
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-11 14:55:33 +0700

    Time: 42 ms (39.17%), Space: 16.8 MB (68.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,6 +1,5 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # open, close = 0, 0
         res = []
         
         def backtrack(comb = "", open = 0, close = 0):
@@ -11,7 +10,7 @@ class Solution:
             if open < n:
                 comb += "("
                 backtrack(comb, open + 1, close)
-                comb = comb[:-1]
+                comb = comb[:-1] # important! clean up after adding (to achieve 2 decisions in decision tree)
 
             if open > close:
                 comb += ")"
