320ab52beaff3a0b7aeae000f5d981657842ee95
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-12 21:29:15 +0700

    Time: 53 ms (31.34%), Space: 19.1 MB (73.73%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Floyd's cycle finding algo
+        # 2 pointer, 1 go 1 step, another one go 2 step at a time
+        # move pointers til pointer 1 reach the end
+        # if there's cycle, pointer 1 and 2 must meet somewhere before the loop end
+        slow, fast = head, head
+        while slow and fast and fast.next:
+            slow = slow.next # 1 step forward
+            fast = fast.next.next # 2 step at a time
+            if slow == fast:
+                return True
+        return False
+        
