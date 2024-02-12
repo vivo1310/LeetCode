ddf3253057178f931586fdfe38734a681a7b5f1a
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-12 21:32:35 +0700

    Time: 48 ms (54.00%), Space: 19.5 MB (62.09%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,11 +10,22 @@ class Solution:
         # 2 pointer, 1 go 1 step, another one go 2 step at a time
         # move pointers til pointer 1 reach the end
         # if there's cycle, pointer 1 and 2 must meet somewhere before the loop end
-        slow, fast = head, head
-        while slow and fast and fast.next:
-            slow = slow.next # 1 step forward
-            fast = fast.next.next # 2 step at a time
-            if slow == fast:
+        # slow, fast = head, head
+        # while slow and fast and fast.next:
+        #     slow = slow.next # 1 step forward
+        #     fast = fast.next.next # 2 step at a time
+        #     if slow == fast:
+        #         return True
+        # return False
+    
+        # Using Hash table
+        hashSet = set()
+        temp = head
+        while temp:
+            if temp in hashSet:
                 return True
+            hashSet.add(temp)
+            temp = temp.next
         return False
+        
         
