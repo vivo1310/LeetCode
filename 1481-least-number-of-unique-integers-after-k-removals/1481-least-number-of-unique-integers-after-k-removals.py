12f7992ae5a1097cb878117166483713ca73bab1
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-16 19:13:07 +0700

    Time: 344 ms (64.88%), Space: 33.3 MB (94.30%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -2,25 +2,19 @@ class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         # create mapping: num -> freq
         # create mapping: freq -> [nums]
-        # decrement k starting from the least frequent
+        # decrement k starting from the least frequent 
+        # O(n*m) where n is len(arr) and m is max frequency
         count = Counter(arr) # O(n)
-        print(count)
         res = len(count)
         
         freqMap = defaultdict(list)
-        for key, val in count.items():
+        for key, val in count.items(): # O(n)
             freqMap[val].append(key)
     
-        print(freqMap)
-
-        # for i in range(1, len(count) + 1): # a number can occur at most n time, eg. arr = [1,1,1]
+        # a number can occur at most n time, eg. arr = [1,1,1]
         i = 1
         temp = i
-        while i < len(count) + 1:
-            # if not freqMap[i]:
-            #     i += 1
-            #     temp = i
-            # temp = i
+        while i < len(count) + 1: # O(n)
             while k > 0 and temp > 0 and freqMap[i]:
                 k -= 1
                 temp -= 1
@@ -30,28 +24,6 @@ class Solution:
                     temp = i
             i += 1
             temp = i
-            # if not freqMap[i]:
-            #     i += 1
-            # else:
-            #     while k > 0 and temp > 0:
-            #         k -= 1
-            #         temp -= 1
-            #         if temp == 0:
-            #             freqMap[i].pop()
-            #             res -= 1
-#             temp = i
-#             while k > 0 and temp > 0 and freqMap[i]:
-#                 k -= 1 # decrement k
-#                 temp -= 1
-#                 if temp == 0:
-#                     removed = freqMap[i].pop() # and remove the number
-#                     print(freqMap, removed, k)
-#                     res -= 1
-#                     # if not freqMap[i]:
-#                     #     freqMap[i-1].append(removed)
-
-#             i += 1
-        print(freqMap)
-        
+                  
         
         return res
