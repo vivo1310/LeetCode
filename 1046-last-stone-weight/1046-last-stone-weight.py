bc880de4b70f0e63bf36224adccd45fc65b6c1ad
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-17 16:33:03 +0700

    Time: 27 ms (96.89%), Space: 16.7 MB (54.45%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -2,18 +2,17 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         # if len(stones) == 1: return stones[0]
         # max heap, take out 2 largest stones, get the diff, push back to heap
+        # https://docs.python.org/3/library/heapq.html
         
-        data = [-x for x in stones]
+        data = [-x for x in stones] # trick to get max heap
         
-        heapq.heapify(data) # to get max heap
+        heapq.heapify(data) # O(n)
         
-        print(data)
-        while len(data) > 1:
+        while len(data) > 1:            
             diff = heapq.heappop(data) - heapq.heappop(data)
             heapq.heappush(data, diff)
-            heapq.heapify(data)
+            # heapq.heapify(data)
             
-        print(data)
         return -data[0]
             
         
