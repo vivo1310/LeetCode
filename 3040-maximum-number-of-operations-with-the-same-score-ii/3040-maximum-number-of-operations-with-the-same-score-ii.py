053f5711e53e55fe18392944b23b4cb5926ee53d
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-18 16:27:11 +0700

    Time: 1874 ms (33.33%), Space: 417.9 MB (33.33%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,30 +1,48 @@
 class Solution:
     def maxOperations(self, nums: List[int]) -> int:
-        def solve(i, j, score):
-            # i at the first, j at the last position
+        n = len(nums)
+        @cache # to not use extra dp map to manually cache
+        def solve(i = 0, j = n-1 , score = 0):
             if i >= j:
                 return 0
-            if (i,j) in dp:
-                return dp[(i,j)]
+            
+            a = b = c = 0
+            if score == 0 or nums[i] + nums[i+1] == score:
+                a = 1 + solve(i + 2, j, nums[i] + nums[i+1])
+            if score == 0 or nums[j] + nums[j-1] == score:
+                b = 1 + solve(i, j - 2, nums[j] + nums[j-1])
+            if score == 0 or nums[i] + nums[j] == score:
+                c = 1 + solve(i + 1, j - 1, nums[i] + nums[j])
+            return max(a,b,c)
+        
+        return solve()
+    
+#         def solve(i, j, score):
+#             # i at the first, j at the last position
+#             if i >= j:
+#                 return 0
+#             if (i,j) in dp:
+#                 return dp[(i,j)]
 
-            ans = 0
-            if nums[i] + nums[i+1] == score: 
-                # Choose the first two elements of nums
-                ans = max(ans, 1 + solve(i + 2, j, nums[i] + nums[i+1]))
-            if nums[j] + nums[j-1] == score:
-                # Choose the last two elements of nums
-                ans = max(ans, 1 + solve(i, j - 2, nums[j] + nums[j-1]))
-            if nums[i] + nums[j] == score:
-                # Choose the first and the last elements of nums
-                ans = max(ans, 1 + solve(i + 1, j - 1, nums[i] + nums[j]))
+#             ans = 0
+#             if nums[i] + nums[i+1] == score: 
+#                 # Choose the first two elements of nums
+#                 ans = max(ans, 1 + solve(i + 2, j, nums[i] + nums[i+1]))
+#             if nums[j] + nums[j-1] == score:
+#                 # Choose the last two elements of nums
+#                 ans = max(ans, 1 + solve(i, j - 2, nums[j] + nums[j-1]))
+#             if nums[i] + nums[j] == score:
+#                 # Choose the first and the last elements of nums
+#                 ans = max(ans, 1 + solve(i + 1, j - 1, nums[i] + nums[j]))
 
-            dp[(i,j)] = ans
-            return ans
+#             dp[(i,j)] = ans
+#             return ans
                           
-        dp = {}
-        i, j = 0, len(nums) - 1
-        res = 1 + max(solve(i + 2, j, nums[i] + nums[i+1]),
-                  solve(i, j - 2, nums[j] + nums[j-1]),
-                  solve(i + 1, j - 1, nums[i] + nums[j]))
+#         dp = {}
+#         i, j = 0, len(nums) - 1
+#         res = 1 + max(solve(i + 2, j, nums[i] + nums[i+1]),
+#                   solve(i, j - 2, nums[j] + nums[j-1]),
+#                   solve(i + 1, j - 1, nums[i] + nums[j]))
 
-        return res
+#         return res
+        
