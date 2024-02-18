01ea33ea6fc28cf6dff001855bd37286bfe23864
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-18 14:38:05 +0700

    Time: 44 ms (100.00%), Space: 16.7 MB (100.00%) - LeetHub

---
+++ 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxOperations(self, nums: List[int]) -> int:
+        res = 0
+        score = nums[0] + nums[1]
+
+        while len(nums) >= 2:
+            currScore = nums[0] + nums[1]
+            if currScore == score:
+                score = currScore
+                nums = nums[2:]
+                res += 1
+            else:
+                break
+
+        return res
+    
+        
