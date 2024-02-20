4859bf9b42ad598567c8d35a75de4a9ba3f96922
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-20 20:27:05 +0700

    Time: 114 ms (55.96%), Space: 17.9 MB (56.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,22 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # constraint 10^4 => aim for O(nlogn)
-        nums.sort()
+#         nums.sort() # O(nlogn)
         
-        for i in range(nums[-1]):
-            if i != nums[i]:
-                return i
-        return nums[-1] + 1
+#         for i in range(nums[-1]):
+#             if i != nums[i]:
+#                 return i
+#         return nums[-1] + 1
+    
+        # Follow up: ask for O(1) space and O(n)?
+        # Sum all numbers
+        n = len(nums)
+        expectedSum = sum([x for x in range(n+1)])
+        # 0 1 2 3 4 -> n=4 -> sum=1+2+3+4=10
+        print(expectedSum)
+        actualSum = 0
+        for n in nums:
+            actualSum += n
+        return expectedSum - actualSum
+        
+        
