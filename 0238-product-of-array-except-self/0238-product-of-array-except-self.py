38ebe7156c1d05fdf5d165822661cc97420d7838
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-22 00:10:01 +0700

    Time: 163 ms (74.86%), Space: 24 MB (66.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,34 +1,51 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # make two passes, first in-order, second in-reverse, to compute products
-        # [1,2,6,24]
-        prefix = []
-        for i in range(len(nums)):
-            if i == 0:
-                prod = nums[i]
-            else:
-                prod = nums[i] * prefix[i-1]
-            prefix.append(prod)
-        # print(prefix)
+        n = len(nums)
+        res = [1]
+        prefix = 1
+        for i in range(1, n): # in-order
+            prefix = prefix * nums[i-1]
+            res.append(prefix)
+            # prefix = prefix * 
+        print(res)
         
-        # [24,24,12,4]
-        postfix = [1 for i in range(len(nums))]
-        for i in range(len(nums) - 1, -1, -1):
-            if i == len(nums) - 1:
-                prod = nums[i]
-            else:
-                prod = nums[i] * postfix[i+1]
-            postfix[i] = prod
-        # print(postfix)
+        postfix = 1
+        for i in range(n-2, -1, -1): # in-reserve
+            postfix = postfix * nums[i+1]
+            res[i] = res[i] * postfix
+        print(res)
         
-        res = []
-        
-        for i in range(len(nums)):
-            if i == 0:
-                res.append(postfix[i + 1])
-            elif i == len(nums) - 1:
-                res.append(prefix[i - 1])
-            else:
-                res.append(prefix[i - 1] * postfix[i + 1])
         return res
+            
+#         # [1,2,6,24]
+#         prefix = []
+#         for i in range(len(nums)):
+#             if i == 0:
+#                 prod = nums[i]
+#             else:
+#                 prod = nums[i] * prefix[i-1]
+#             prefix.append(prod)
+#         # print(prefix)
+        
+#         # [24,24,12,4]
+#         postfix = [1 for i in range(len(nums))]
+#         for i in range(len(nums) - 1, -1, -1):
+#             if i == len(nums) - 1:
+#                 prod = nums[i]
+#             else:
+#                 prod = nums[i] * postfix[i+1]
+#             postfix[i] = prod
+#         # print(postfix)
+        
+#         res = []
+        
+#         for i in range(len(nums)):
+#             if i == 0:
+#                 res.append(postfix[i + 1])
+#             elif i == len(nums) - 1:
+#                 res.append(prefix[i - 1])
+#             else:
+#                 res.append(prefix[i - 1] * postfix[i + 1])
+#         return res
      
