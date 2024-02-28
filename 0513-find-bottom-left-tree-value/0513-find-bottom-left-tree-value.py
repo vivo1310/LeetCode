c62bf700cdd3fe8d3016426247b4a47239aaaec1
Vi <86089777+vivo1310@users.noreply.github.com>
2024-02-28 11:49:45 +0700

    Time: 45 ms (39.52%), Space: 18.3 MB (52.22%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -6,21 +6,33 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:    
-        best = 0
-        maxDepth = -1
+#         best = 0
+#         maxDepth = -1
         
-        def dfs(node, depth):
-            if not node: return
+#         def dfs(node, depth):
+#             if not node: return
             
-            nonlocal maxDepth
-            nonlocal best
-            if depth > maxDepth:
-                maxDepth = depth
-                best = node.val
+#             nonlocal maxDepth
+#             nonlocal best
+#             if depth > maxDepth:
+#                 maxDepth = depth
+#                 best = node.val
                 
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
-            # return
+#             dfs(node.left, depth + 1)
+#             dfs(node.right, depth + 1)
+#             # return
             
-        dfs(root, 0)
-        return best
+#         dfs(root, 0)
+#         return best
+
+        # BFS, the last item in the queue is the left most node
+        q = deque()
+        curr = root
+        q.append(curr)
+        while q:
+            curr = q.popleft()
+            if curr.right: 
+                q.append(curr.right)
+            if curr.left:
+                q.append(curr.left)
+        return curr.val
