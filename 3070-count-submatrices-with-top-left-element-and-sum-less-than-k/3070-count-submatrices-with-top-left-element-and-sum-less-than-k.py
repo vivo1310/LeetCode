2e86dfe81308e82ec204819293a2d2fefd33b5cd
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-03 19:38:15 +0700

    Time: 1613 ms (50.00%), Space: 66.1 MB (50.00%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,30 +1,41 @@
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        # create a prefix sum matrix
+        # O(R*C) for time and O(1) for space
+        # create a prefix sum matrix 
         # keep count of sum <= k
-        R, C = len(grid), len(grid[0])
-        
-        ps = [[0] * (C + 1) for _ in range(R + 1)]
-        
-        for i in range(1, R + 1):
-            for j in range(1, C + 1):
-                ps[i][j] = grid[i-1][j-1] + ps[i][j-1]
-        # print(ps)
-        count = 0
-        for i in range(1, R + 1):
-            for j in range(1, C + 1):
-                ps[i][j] += ps[i-1][j]
-                if ps[i][j] <= k:
-                    count += 1
-        
-        # count = 0
-        # for i in range(1, R + 1):
-        #     for j in range(1, C + 1):
-        #         if ps[i][j] <= k:
-        #             count += 1
+#         R, C = len(grid), len(grid[0])
+            
+#         for i in range(R):
+#             for j in range(1, C):
+#                 grid[i][j] += grid[i][j-1]
+
+#         for j in range(C):
+#             for i in range(1, R):
+#                 grid[i][j] += grid[i-1][j]
                 
-        # print(ps)
-        
-        
-        return count
-        
+#         count = 0
+#         for i in range(R):
+#             for j in range(C):
+#                 if grid[i][j] <= k:
+#                     count += 1
+#         return count
+
+        # better version
+        R, C = len(grid), len(grid[0])
+        c = 0
+        s = [[0] * C for _ in range(R)]
+        for i in range(R):
+            for j in range(C):
+                q = grid[i][j]
+                if i > 0:
+                    q += s[i-1][j]
+                if j > 0:
+                    q += s[i][j-1]
+                if i > 0 and j > 0:
+                    q -= s[i-1][j-1]
+                s[i][j] = q
+                if q <= k:
+                    c += 1
+        # print(s)
+        return c
+      
