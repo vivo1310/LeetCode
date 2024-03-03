4f738169b05ff7ba309b9e920eb2bffdbbb6ae08
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-03 19:28:34 +0700

    Time: 1679 ms (50.00%), Space: 65.8 MB (50.00%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,28 +1,24 @@
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        # create a prefix sum matrix
+        # O(R*C) for time and O(1) for space
+        # create a prefix sum matrix 
         # keep count of sum <= k
         R, C = len(grid), len(grid[0])
+            
         
-        ps = [[0] * (C + 1) for _ in range(R + 1)]
-        
-        for i in range(1, R + 1):
-            for j in range(1, C + 1):
-                ps[i][j] = grid[i-1][j-1] + ps[i][j-1]
-        # print(ps)
-        
-        for i in range(1, R + 1):
-            for j in range(1, C + 1):
-                ps[i][j] += ps[i-1][j]
-        
-        count = 0
-        for i in range(1, R + 1):
-            for j in range(1, C + 1):
-                if ps[i][j] <= k:
-                    count += 1
+        for i in range(R):
+            for j in range(1, C):
+                grid[i][j] += grid[i][j-1]
+
+        for j in range(C):
+            for i in range(1, R):
+                grid[i][j] += grid[i-1][j]
                 
-        # print(ps)
-        
-        
+        count = 0
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] <= k:
+                    count += 1
         return count
+      
         
