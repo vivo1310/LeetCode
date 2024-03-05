65a13786c0fb6907faba8068fbad65ba848ea4d9
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-05 09:06:03 +0700

    Time: 74 ms (46.92%), Space: 17.6 MB (42.65%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -1,15 +1,29 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        i, j = 0, len(s) - 1
-        while i < j and s[i] == s[j]:
-            c = s[i] # character of similar prefix
-            while i <= j and s[i] == c:
-                i += 1
-            while j > i and s[j] == c:
-                j -= 1
+        # using queue
+        q = collections.deque(s)
+        
+        while len(q) > 1 and q[0] == q[-1]:
+            c = q[0]
             
-            # print(i,j,c)
-        return j - i + 1
+            while len(q) > 0 and q[0] == c:
+                q.popleft()
+            while len(q) > 0 and q[-1] == c:
+                q.pop()
+            
+        return len(q)
+    
+        # 2 pointers
+        # i, j = 0, len(s) - 1
+        # while i < j and s[i] == s[j]:
+        #     c = s[i] # character of similar prefix
+        #     while i <= j and s[i] == c:
+        #         i += 1
+        #     while j > i and s[j] == c:
+        #         j -= 1
+        # return j - i + 1
+    
+        # first approach, old code
         # while i < j:
         #     if s[i] == s[j]:
         #         while i + 1 < len(s) and s[i +1 ] == s[i]:
