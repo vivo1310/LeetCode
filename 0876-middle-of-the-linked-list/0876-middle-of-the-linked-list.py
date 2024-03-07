1c53493a76dbde79d449ef6b9898ceb479c6c94f
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-07 10:41:43 +0700

    Time: 50 ms (5.12%), Space: 16.5 MB (89.17%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,12 +5,24 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        countPointer = head
-        count = 0 # count number of node in this linked list
-        while countPointer:
-            countPointer = countPointer.next
-            count += 1
-        res = head
-        for i in range(count // 2):
-            res = res.next
-        return res
+        # 1st method: Traverse once to count number of nodes in the list
+        # countPointer = head
+        # count = 0 # count number of node in this linked list
+        # while countPointer:
+        #     countPointer = countPointer.next
+        #     count += 1
+        # res = head
+        # for i in range(count // 2):
+        #     res = res.next
+        # return res
+        
+        # 2nd method: Slow 1 step, fast 2 steps pointer
+        # Slow will point to middle when fast reach the end
+        
+        slow, fast = head, head
+        
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
