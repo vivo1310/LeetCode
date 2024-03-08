8cf0f3cbf962714711e321d188562796042feba4
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-08 08:13:20 +0700

    Time: 46 ms (41.26%), Space: 16.5 MB (83.74%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -1,13 +1,31 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        counter = collections.Counter(nums)
-        count = 0
+        # O(n) Time O(n) Space
+#         counter = collections.Counter(nums)
+#         count = 0
+#         maxFreq = 0
+#         for v in counter.values():
+#             if v > maxFreq:
+#                 maxFreq = v
+#                 count = v
+#             elif v == maxFreq:
+#                 count += v
+                
+#         return count
+    
+        
+        hm = {}
         maxFreq = 0
-        for v in counter.values():
+        count = 0
+        for n in nums:
+            if n not in hm:
+                hm[n] = 1
+            else:
+                hm[n] += 1
+            v = hm[n]
             if v > maxFreq:
                 maxFreq = v
                 count = v
             elif v == maxFreq:
                 count += v
-                
         return count
