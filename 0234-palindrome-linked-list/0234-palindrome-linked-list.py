b119dd3fdf92b56ddacd293f50ef1d731605cae2
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-22 10:30:16 +0700

    Time: 295 ms (49.62%), Space: 31.9 MB (93.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,24 +3,51 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         #convert to list
+#         ls = []
+#         cur = head
+#         while cur:
+#             ls.append(cur.val)
+#             if cur.next:
+#                 cur = cur.next
+#             else:
+#                 break
+        
+#         #check if list is palindrom
+#         i,j = 0, len(ls) - 1
+#         while i < j:
+#             if ls[i] != ls[j]:
+#                 return False
+#             i += 1
+#             j -= 1
+#         return True
+        
+        
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        #convert to list
-        ls = []
-        cur = head
-        while cur:
-            ls.append(cur.val)
-            if cur.next:
-                cur = cur.next
-            else:
-                break
+    def isPalindrome(self, head: Optional[ListNode]) -> bool: # O(n) time, O(n/2) space
+        def reverse(head):
+            prevNode = None
+            currNode = head
+            while currNode:
+                nextNode = currNode.next
+                currNode.next = prevNode
+                prevNode = currNode
+                currNode = nextNode
+            return prevNode
         
-        #check if list is palindrom
-        i,j = 0, len(ls) - 1
-        while i < j:
-            if ls[i] != ls[j]:
+        # move slow pointer to middle node
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half of linked list
+        reversedHalf = reverse(slow)
+        while reversedHalf:
+            if head.val != reversedHalf.val:
                 return False
-            i += 1
-            j -= 1
+            head = head.next
+            reversedHalf = reversedHalf.next
         return True
-        
