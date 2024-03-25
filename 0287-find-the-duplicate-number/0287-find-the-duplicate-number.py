c5ec66ca822312733abac9c45ec278d290d943ab
Vi <86089777+vivo1310@users.noreply.github.com>
2024-03-25 13:21:53 +0700

    Time: 511 ms (17.46%), Space: 30.4 MB (84.50%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,13 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # 1.hashset - extra space
-        # 2.linked list cycle
-        for i in range(len(nums)):
-            ind = abs(nums[i])
-            # print(i,ind)
-
-            if nums[ind] < 0:
-                return ind
-            nums[ind] = -nums[ind]
-            # print(nums)
+        for x in nums:
+            x = abs(x)
+            if nums[x-1] < 0:
+                return x
+            nums[x-1] *= -1 # mark visited
         return -1
+#         for i in range(len(nums)):
+#             ind = abs(nums[i])
+#             if nums[ind] < 0:
+#                 return ind
+#             nums[ind] = -nums[ind]
+#         return -1
