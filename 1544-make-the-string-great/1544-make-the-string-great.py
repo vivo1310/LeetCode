2e784ce82394b13fe25dbf7b06071971dbbda214
Vi <86089777+vivo1310@users.noreply.github.com>
2024-04-05 15:57:58 +0700

    Time: 27 ms (97.77%), Space: 16.6 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,11 +2,9 @@ class Solution:
     def makeGood(self, s: str) -> str:
         i = 0
         while 0 <= i <= len(s) - 2:
-            if abs(ord(s[i]) - ord(s[i+1])) == 32:
-                # print('bef', s, i)
+            if abs(ord(s[i]) - ord(s[i+1])) == 32: # s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
                 s = s[:i] + s[i+2:]
                 i = 0
-                # print('aft',s,i)
             else:
                 i += 1
         return s
