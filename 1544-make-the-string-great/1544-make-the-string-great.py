32551cbe97d0ab2c7a9892ca91344f7763762009
Vi <86089777+vivo1310@users.noreply.github.com>
2024-04-05 15:58:52 +0700

    Time: 41 ms (34.80%), Space: 16.7 MB (11.30%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -4,7 +4,9 @@ class Solution:
         while 0 <= i <= len(s) - 2:
             if abs(ord(s[i]) - ord(s[i+1])) == 32: # s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
                 s = s[:i] + s[i+2:]
-                i = 0
+                # i = 0
+                if i > 0:
+                    i -= 1
             else:
                 i += 1
         return s
