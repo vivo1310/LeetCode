57d3e31f42d9670e073f5d3787d39aad05f33976
Vi <86089777+vivo1310@users.noreply.github.com>
2024-04-16 17:47:41 +0700

    Time: 43 ms (78.75%), Space: 17.9 MB (32.50%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
@@ -6,11 +6,22 @@
 #         self.right = right
 class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+#         If depth == 1 that means there is no depth depth - 1 at all, then create a tree node with value val as the new root of the whole original tree, and the original tree is the new root's left subtree.
         if depth == 1: return TreeNode(val, root, None)
-        elif depth == 2:
-            root.left = TreeNode(val, root.left, None)
-            root.right = TreeNode(val, None, root.right)
-        else:
-            if root.left: self.addOneRow(root.left, val, depth-1)
-            if root.right: self.addOneRow(root.right, val, depth-1)
-        return root
+    
+        def traverse(node, currDepth):
+            if not node:
+                return
+#             Given the integer depth, for each not null tree node cur at the depth depth - 1, create two tree nodes with value val as cur's left subtree root and right subtree root.
+            if currDepth == depth - 1: # start to add one row
+                currLeft = node.left
+                currRight = node.right
+                node.left = TreeNode(val, left = currLeft)
+                node.right = TreeNode(val, right = currRight)
+                return root
+            traverse(node.left, currDepth + 1)
+            traverse(node.right, currDepth + 1)
+            return node
+        
+        return traverse(root, 1)
+                
