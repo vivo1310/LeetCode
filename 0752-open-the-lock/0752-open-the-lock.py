f66ee35f56f2a9124c36fdd87d0bc9b429fd0068
Vi <86089777+vivo1310@users.noreply.github.com>
2024-04-22 10:31:08 +0700

    Time: 421 ms (70.37%), Space: 18.6 MB (11.17%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,23 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        marker, depth = 'x', -1
+        visited, q = set(deadends), deque(['0000'])
+
+        while q:
+            size = len(q)
+            depth += 1
+            for _ in range(size):
+                node = q.popleft()
+                if node == target: return depth
+                if node in visited: continue
+                visited.add(node)
+                q.extend(self.successors(node))
+        return -1
+
+    def successors(self, src):
+        res = []
+        for i, ch in enumerate(src):
+            num = int(ch)
+            res.append(src[:i] + str((num - 1) % 10) + src[i+1:])
+            res.append(src[:i] + str((num + 1) % 10) + src[i+1:])
+        return res
